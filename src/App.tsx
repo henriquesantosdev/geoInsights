452fbfa41944f98b85589c65b8d8e192e7996a18
henriquesantosdev <henriquesantos2277@gmail.com>
2024-09-24 00:42:27 -0300

    :tada: Starting project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { SimpleMapPernambuco } from "./components/simpleMapPernambuco"
+
+function App() {
+  return (
+    <>
+    <h1>War room</h1>
+    <SimpleMapPernambuco/>
+    </>
+  )
+}
+
+export default App
